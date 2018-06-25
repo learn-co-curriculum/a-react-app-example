73c6e64bdd0068a562f9884fa64338258d9cab88
maxwell <github email address>
2018-06-25 12:23:53 -0400

    adding tests, unfinished

---
+++ test/index-test.js
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ExampleComponent } from '../src/index'
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('OlderCoaster', () => {
+  let wrapper;
+
+  before(() => {
+    wrapper = shallow(React.createElement(ExampleComponent));
+  });
+
+  it('should have the right DOM markup', () => {
+    expect(wrapper.html()).to.equal(`<div class="oldercoaster"><p>Two grannies having the time of their life!</p><p>Passengers:</p><ul><li>Agnes</li><li>Muriel</li></ul></div>`);
+  });
+});
+
+// describe('InFrontOfYou', () => {
+//   let wrapper;
+//
+//   before(() => {
+//     wrapper = shallow(React.createElement(InFrontOfYou));
+//   });
+//
+//   it('renders withouth crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<InFrontOfYou />, div);
+//   });
+//
+//   it('should have the right DOM markup', () => {
+//     expect(wrapper.html()).to.equal(`<div><p>You shouldn&#x27;t look too far.</p><p>Sometimes, the solution is right in front of you.</p></div>`);
+//   });
+// });
+//
+// describe('ButcherShop', () => {
+//   let wrapper;
+//
+//   before(() => {
+//     wrapper = shallow(React.createElement(ButcherShop));
+//   });
+//
+//   it('renders withouth crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<ButcherShop />, div);
+//   });
+//
+//   it('should have the right DOM markup', () => {
+//     expect(wrapper.html()).to.equal(`<div class="butcher-shop"><p>Hello! We have the following products for sale today:</p><ul><li>Tenderloin</li><li>Short ribs</li><li>Beef shin</li><li>Ribeye</li></ul></div>`);
+//   });
+// });

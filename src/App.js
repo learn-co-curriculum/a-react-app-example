1c3f2c8efb1fb763cde2482968420caa94ac9edc
Maxwell Benton <maxwellbenton@gmail.com>
2019-09-19 18:09:36 -0400

    fix errors in text content

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ class App extends Component {
           {moment().format('MMMM Do YYYY, hh:mm:ss a')}
         </header>
         <p className="App-intro">
-          In React apps, we write JSX - it looks like HTML, and uses a lot HTML syntax.
+          In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
           JSX lets us include JavaScript functions right along with the HTML, and also
-          allows us to add in components, which are separate, self-contained chunks of of JSX.
+          allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
         <ExampleComponent />
 

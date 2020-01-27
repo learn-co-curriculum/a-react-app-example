50577c74d20cd8596a0f13bcfd8a4e9fa9578f5f
Sylwia Vargas <45401242+sylwiavargas@users.noreply.github.com>
2020-01-27 11:10:05 -0500

    make alt text more descriptive

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -5,7 +5,7 @@ class ExampleComponent extends Component {
   render() {
     return (
       <div className="example">
-        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="whoa"/>
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
         <p>Whoa!</p>
       </div>
     );
